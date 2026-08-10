22f215b37cbf4c9d0f8231a746365ada089ca0d5
Phong Nguyen Dinh Hai <bunsomui@gmail.com>
2026-08-10 23:48:10 +0700

    Update e2ee.js

--- js/e2ee.js
+++ js/e2ee.js
@@ -248,7 +248,7 @@
             );
             const bytes = new Uint8Array(hashBuf);
             let digits = "";
-            for (let i = 0; i < 15; i++) {
+            for (let i = 0; i < 30; i++) {
                 digits += String(bytes[i] % 10);
                 digits += String(Math.floor(bytes[i] / 10) % 10);
             }
